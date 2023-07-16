ff19f8296ed340dbd0c91de54ab5c6cda2e0bdba
maciejkuran <maciejkuran@gmail.com>
2023-07-16 18:40:49 +0200

    Added validation for sign up form

---
+++ pages/api/sign-up.tsx
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import rateLimiterMiddleware from '@/rateLimitedMiddleware';
+import validateEmail from '@/utils/validateEmail';
+import validatePassword from '@/utils/validatePassword';
+
+const rateLimiter = {};
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password, confirmPassword } = req.body;
+
+  //Check rate limit
+  const rateLimitOk = rateLimiterMiddleware(req, res, rateLimiter);
+
+  if (!rateLimitOk) return;
+
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: 'Invalid request method. Accepted: POST' });
+  }
+
+  //Validate passwords match
+  if (password.trim() !== confirmPassword.trim()) {
+    return res.status(400).json({ error: 'Passwords do not match.' });
+  }
+
+  //Validate email address
+  const correctEmail = validateEmail(email, res);
+  if (!correctEmail) return;
+
+  //Validate password
+  const correctPassword = validatePassword(password, res);
+  if (!correctPassword) return;
+
+  //Success
+  return res.status(200).json({ success: 'Success.' });
+};
+
+export default handler;
