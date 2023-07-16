9b5db5baf8ae0c35ac917fb219c691bd6ff9c146
maciejkuran <maciejkuran@gmail.com>
2023-07-16 20:57:22 +0200

    Added information about ongoing work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', fontFamily: 'monospace' }}>
+        👋 Currently, the API documentation is being created. Please be patient!
+      </h1>
+      <p style={{ textAlign: 'center', fontSize: '50px' }}>🖤</p>
+    </div>
+  );
 }
