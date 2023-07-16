ef9127893fd1a5644a8b16bafc2d8443723f5031
maciejkuran <maciejkuran@gmail.com>
2023-07-16 20:46:20 +0200

    Wrapped app with a LayoutWrapper components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import LayoutWrapper from '@/components/Layout/LayoutWrapper';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutWrapper>
+      <Component {...pageProps} />
+    </LayoutWrapper>
+  );
 }
