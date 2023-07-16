9e5bbdb529d58a14bcc1d7e17a50840db26e5e5d
maciejkuran <maciejkuran@gmail.com>
2023-07-16 21:05:36 +0200

    Added extra check for OPTIONS to prevent CORS issues.

--- pages/api/sign-in.tsx
+++ pages/api/sign-in.tsx
@@ -13,6 +13,13 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
 
   if (!rateLimitOk) return;
 
+  //Preventing CORS issues.If you try to send a POST etc.. request , the preflight will send it’s ‘first army’ to check the ‘battle field’. But this army is not the request itself, but an OPTION request. That’s why in our API we need to handle OPTION request.
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK',
+    });
+  }
+
   if (req.method !== 'POST') {
     return res.status(400).json({ error: 'Invalid request method. Accepted: POST' });
   }
