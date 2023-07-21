d72d413ffe0b0b654658ff2b5d2e205ab335f0d5
maciejkuran <maciejkuran@gmail.com>
2023-07-21 17:36:19 +0200

    Added ApiEndpoints components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Introduction from '@/components/Introduction/Introduction';
+import ApiEndpoints from '@/components/ApiEndpoints/ApiEndpoints';
 
 export default function Home() {
   return (
     <>
       <Introduction />
+      <ApiEndpoints />
     </>
   );
 }
