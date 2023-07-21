12df417f17249b481cb5938eed85db3021087da7
maciejkuran <maciejkuran@gmail.com>
2023-07-21 17:34:48 +0200

    Added text to paragraph.

--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -3,12 +3,14 @@ import classes from './Introduction.module.scss';
 
 const Introduction = (): ReactNode => {
   return (
-    <section className={classes.introduction}>
+    <section id="introduction" className={classes.introduction}>
       <h1>Forms Validation REST API Documentation</h1>
       <p>
-        You don&lsquo;t have to worry about form validation anymore, and write boilerplate code 😩.
-        This API handles validation out-of-the-box 📦! It&lsquo;s as simple as that.
+        Introducing the Lightning Fast Free Form Validation REST API! 🚀. You don&lsquo;t have to
+        worry about form validation anymore, and write boilerplate code 😩. This API handles
+        validation out-of-the-box 📦! It&lsquo;s as simple as that.
       </p>
+      <p className={classes['introduction__version']}>Current version: 1.0</p>
       <p>
         🔗 API BASE URL:{' '}
         <code className="code">https://forms-validation-api.vercel.app/api/v1.0</code>
